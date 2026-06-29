3d8ff52da115b0c097f5e65f08647a65d42afce4
duc2-byte <anhnguyenduc28900@gmail.com>
2026-06-29 10:56:21 +0700

    Add lesson3.cs file with basic program structure

---
+++ week1/lesson3.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Name: Codelearn");
+            Console.WriteLine("Date of birth: 2019");
+        }
+    }
+}
